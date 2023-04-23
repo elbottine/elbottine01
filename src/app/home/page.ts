54ae471c939dd036ffac7d9da6d6b94242b9fb2d
Saïd Chikh <said.chikh@ores.be>
2023-04-23 22:56:02 +0200

    commit

--- src/app/home/page.ts
+++ src/app/home/page.ts
@@ -14,13 +14,19 @@ import { Component } from '@angular/core';
     <div class="col-9">
             <!-- <h3>Présentation</h3> -->
         <p>	
-        Notre club de marches fait parti de la Fédération Francophone Belge de Marches populaires ( FFBMP - Matricule HT 089)
+        Notre club de marches fait parti de la Fédération Francophone Belge de Marches populaires ( FFBMP - Matricule HT 089).
         </p>
         <p>	
         Celui ci a été créé le 1 er janvier 2021 avec 5 fondateurs principaux sur l initiative de Patrick Joly et Fernand Ruelle, se joignent à eux:  Patrice Mouton,  Jean Pierre Jasmin et Myriam Duvivier.
         </p>
         <p>	
-        Vous trouverez sur ce site toutes les informations sur les activités de notre club ainsi que nos marches annuelles..
+        Vous trouverez sur ce site toutes les informations sur les activités de notre club ainsi que nos marches annuelles.
+        </p>
+        <p>	
+        Nous organisons une marche d'entrainement d'environ 8 km tout les mercredis.
+        </p>
+        <p>	
+        Bienvenue
         </p>
     </div>
     <div class="col-3">
