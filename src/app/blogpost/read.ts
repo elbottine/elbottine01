cd2756690f42770cdc328f777ba79d21b310bb9d
Saïd Chikh <said.chikh@ores.be>
2023-05-18 09:05:10 +0200

    commit

--- src/app/blogpost/read.ts
+++ src/app/blogpost/read.ts
@@ -17,7 +17,7 @@ import { AuthService } from '../auth/auth.service';
 		<div class="card-text" [innerHTML]="model.text"></div>
 
         <div class="w-auto" style="min-height: 0px;background-color: #eee;">
-            <img *ngFor="let path of paths" [src]="path" class="preview" style="width: 200px; height: 200px; object-fit: cover;" />
+            <img *ngFor="let path of model.paths" [src]="path" class="preview" style="width: 200px; height: 200px; object-fit: cover;" />
         </div>        
 
         <div class="d-flex align-items-end">
@@ -37,12 +37,10 @@ export class BlogpostReadComponent implements OnInit {
 
     constructor(
         private blogpostService: BlogpostService,
-        private dialogService: DialogService,
         private activatedRoute: ActivatedRoute,
         private accountService: AuthService
     ) { }
 
-    private modelCopy: Blogpost;
     model: Blogpost;
     id: string;
 
@@ -56,90 +54,14 @@ export class BlogpostReadComponent implements OnInit {
     }
 
     private get() {
-        if (this.id) {
-            this.blogpostService
-                .get(this.id)
-                .subscribe((b: Blogpost) => {
-                    this.modelCopy = b.clone();
-                    this.model = b;
-                });
-        } else {
-            const blogpost = new Blogpost();
-            this.modelCopy = blogpost.clone();
-            this.model = blogpost;
-        }
-    }
-
-    apply(): void {
-        const model = this.model.clone();
-        this.blogpostService.upsert(model).subscribe(
-            () => {
-                this.dialogService.success('Evénement sauvegardé');
-                this.model = model;
-            },
-            (error: any) => {
-                error = error.error || error;
-                const message = error.ExceptionMessage || error.message || error.Message;
-                this.dialogService.error(message, 'Erreur technique');
-            }
-        );
-    }
-
-    deleteBlogpost(node: Blogpost, $event: any): void {
-        $event.stopPropagation();
-        this.dialogService.confirm('Information', "Suprimer l'événement ?")
-            .then(result => result && this.delete(node));
+        this.blogpostService
+            .get(this.id)
+            .subscribe((b: Blogpost) => {
+                this.model = b;
+            });
     }
 
     canEdit(blogpost: Blogpost) {
         return blogpost.id && this.accountService.isLogged;
     }
-
-    private delete(node: Blogpost): void {
-        this.blogpostService.delete(node.id)
-            .subscribe(
-                () => {
-                    this.dialogService.success('Evénement suprimée');
-                }
-            );
-    }
-
-    get dirty(): boolean {
-        const m: any = this.model;
-        const c: any = this.modelCopy;
-        return Object.keys(this.model).some(p => m[p] !== c[p]);
-    }
-
-    get paths(): string[] {
-        return this.model.paths && []
-    }
-
-    config: AngularEditorConfig = {
-        editable: true,
-        spellcheck: true,
-        // height: '15rem',
-        // minHeight: '5rem',
-//      placeholder: 'Enter text here...',
-        translate: 'no',
-        defaultParagraphSeparator: 'p',
-//        defaultFontName: 'Arial',
-        toolbarHiddenButtons: [
-            ['bold']
-        ],
-        customClasses: [
-            {
-                name: "quote",
-                class: "quote",
-            },
-            {
-                name: 'redText',
-                class: 'redText'
-            },
-            {
-                name: "titleText",
-                class: "titleText",
-                tag: "h1",
-            },
-        ]
-    };
 }
