c81201ad451de035b893d5599757d99df5e7cc9a
Saïd Chikh <said.chikh@ores.be>
2023-08-23 22:25:13 +0200

    update

--- src/app/auth/after-login.service.ts
+++ src/app/auth/after-login.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AfterLoginService implements CanActivate{
+export class AfterLoginService {
 
   canActivate(route:ActivatedRouteSnapshot,state:RouterStateSnapshot):boolean | Observable<boolean>{
     return this.Token.loggedIn();
--- src/app/auth/before-login.service.ts
+++ src/app/auth/before-login.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { TokenService } from './token.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BeforeLoginService implements CanActivate{
+export class BeforeLoginService {
 
   canActivate(route:ActivatedRouteSnapshot,state:RouterStateSnapshot):boolean | Observable<boolean>{
     return !this.Token.loggedIn();
