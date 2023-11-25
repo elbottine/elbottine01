2661a7a6fa7afdc4a77b458baf05ba0dd6ddae4e
Saïd Chikh <said.chikh@ores.be>
2023-11-25 11:35:40 +0100

    update

--- api/account/index.ts
+++ api/account/index.ts
@@ -50,7 +50,7 @@ function validateAccount(userId: string, password: string) {
     var h = hash(password);
     if (userId === 'said'   && h === '98a904cc8e43e1226e9a3388772f99c31c1fdd6f') return true;
     if (userId === 'ingrid' && h === '4bade81175f7af909e0eeac262905eb44961444d') return true;
-    if (userId === 'myriam' && h === '5891bb551084b46bd17e5c1cca496c43a99be933') return true;
+    //if (userId === 'myriam' && h === '5891bb551084b46bd17e5c1cca496c43a99be933') return true;
     return false;
 }
 
